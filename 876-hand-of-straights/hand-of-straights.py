6bce73a86d2e38f67b67d4ded85de637f2151e21
Shireen305 <137711113+Shireen305@users.noreply.github.com>
2023-07-22 17:18:26 -0400

    Time: 223 ms (42.87%) | Memory: 18.2 MB (36.21%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.py
+++ 876-hand-of-straights/hand-of-straights.py
@@ -1,28 +1,21 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        if len(hand)%groupSize!=0:
+        if len(hand) % groupSize != 0:
             return False
-        no_groups=len(hand)//groupSize
         hand.sort()
-        if no_groups==1:
-            for i in range(len(hand)-1):
-                if hand[i]!=hand[i+1]-1:
-                    return False
-            return True
-        dict_count = {}
-        for n in hand:
-            dict_count[n] = 1 + dict_count.get(n, 0)
-        #print(dict_count)
-        for i in range(len(hand)):
-            num=hand[i]
-            if dict_count[num]<=0:
-                continue
-            for j in range(num,num+groupSize):
-                if j not in dict_count:
-                    return False
-                if dict_count[j]>0:
-                    dict_count[j]-=1
-                else:
-                    #print(dict_count)
-                    return False
+        d = {}
+        for key in hand:
+            if key not in d:
+                d[key] = 0
+            d[key] += 1
+        print(d)
+        for num in hand:
+            if d[num] > 0:
+                for i in range(groupSize):
+                    if num+i not in d:
+                        return False
+                    if d[num+i] > 0:
+                        d[num+i] -= 1
+                    else:
+                        return False
         return True
